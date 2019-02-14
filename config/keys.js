afd2a17ea7dd87cf32496603592096999a98e524
Brandon Turner <brandon88@gmail.com>
2019-02-14 12:38:27 -0800

    mongo push

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://vanride-shard-00-00-laq4g.mongodb.net:27017,vanride-shard-00-01-laq4g.mongodb.net:27017,vanride-shard-00-02-laq4g.mongodb.net:27017/test?replicaSet=vanride-shard-0 --ssl --authenticationDatabase admin --username brandonjturner --password Turner@88"
+};
